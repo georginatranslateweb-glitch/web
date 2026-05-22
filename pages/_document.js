0e4c93022cc97706df8bfc88eab2ede13200e998
thenergybooster <thenergybooster@gmail.com>
2026-05-22 12:22:00 +1000

    favicon updated

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>Georgina Robledo - Translation Services </title>
-        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <body data-theme="light" data-menu="fixed" data-footer-effect="on">
         <Main />
