ab1c3a2570285c901a403a24a7bed8ee2cac383d
thenergybooster <thenergybooster@gmail.com>
2026-06-16 19:30:08 +1000

    css clean

--- pages/_app.js
+++ pages/_app.js
@@ -30,6 +30,7 @@ import "../assets/css/mobile-header-drawer.css";
 import "../assets/css/tailwind.css";
 import "../assets/css/how-it-works-timeline.css";
 import "jarallax/dist/jarallax.min.css";
+import "../assets/css/project-single-hero.css";
 import "../assets/css/services-banner.css";
 import "../assets/css/google-reviews.css";
 
